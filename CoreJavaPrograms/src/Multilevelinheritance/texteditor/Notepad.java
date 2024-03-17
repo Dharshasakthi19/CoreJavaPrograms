742a7ce8c2a4a947778359a746b6c8192125f937
PRIYADHARSHINI <PRIYADHARSHINI@dharsha-gowtham>
2024-03-17 21:41:23 +0530

    my multilevelinheritance texteditor program

---
+++ CoreJavaPrograms/src/Multilevelinheritance/texteditor/Notepad.java
@@ -0,0 +1,16 @@
+//Program to define class Notepad 
+package Multilevelinheritance.texteditor;
+
+public class Notepad {
+protected String content;
+
+public Notepad() {
+	this.content="";
+}
+public void write (String text){
+	content+=text;
+}
+public void displaycontent() {
+	System.out.println("Notepad content:"+content);
+}
+}
